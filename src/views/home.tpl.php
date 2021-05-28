f1c8e5121880cb3759104bc57e2fe06f5d233e70
Nikolaos Anastasopoulos <54608658+koukoudo@users.noreply.github.com>
2021-05-28 13:35:27 +0200

    Delete src directory

--- src/views/home.tpl.php
+++
@@ -1,4 +0,0 @@
-<div class="container-fluid m-0 p-5 d-flex flex-column justify-content-center align-items-center">
-    <h1 class="m-3 p-3">WELCOME!</h1>
-    <a class="btn btn-danger" href="product/list">View Products</a>
-</div>
